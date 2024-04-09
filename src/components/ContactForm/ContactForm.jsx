6a2efd38ebca746f723b7a34fd68178087dcc590
Dmytro Kasko <dimoks77@gmail.com>
2024-04-09 18:33:55 +0200

    the end?

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,53 +1,49 @@
-import { useId } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import css from './ContactForm.module.css';
-import { nanoid } from 'nanoid';
+import { useId } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { nanoid } from "nanoid";
+import * as Yup from "yup";
+import css from "./ContactForm.module.css";
 
-const userScheme = Yup.object().shape({
-    name: Yup.string()
-        .min(3, "Too Short!")
-        .max(50, "Too Long!")
-        .required("This is a requarid field"),
-    number: Yup.string()
-        .min(3, "Too Short!")
-        .max(50, "Too Long!")
-        .required('This is a required field'),
+const contactScheme = Yup.object().shape({
+  name: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("This is a requarid field"),
+  number: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("This is a required field"),
 });
 
 export const ContactForm = ({ onAdd }) => {
-    const nameFieldId = useId();
-    const numberFieldId = useId();
+  const nameFieldId = useId();
+  const numberFieldId = useId();
 
-    return ( 
-    <Formik 
+  return (
+    <Formik
       initialValues={{
-        username: '',
+        name: '',
         number: '',
-    }}
-    validationSchema={userScheme} 
-    onSubmit={(values, actions) => {
-        onAdd({ id: nanoid(), ...values});
-        actions.reset.form;
-    }}
+      }}
+      validationSchema={contactScheme}
+      onSubmit={(values, actions) => {
+        onAdd({ id: nanoid(), ...values });
+        actions.resetForm();
+      }}
     >
-        <Form className={css.form}>
-            <div className={css.formGroup}>
-                <label htmlFor={nameFieldId}>Name:</label>
-                <Field className={css.field} type="text" name="name" id={nameFieldId} />
-                <ErrorMessage name="name"  className={css.error} component="span" />
-            </div>
+      <Form className={css.form}>
+        <div className={css.formGroup}>
+          <label htmlFor={nameFieldId}>Name:</label>
+          <Field className={css.field} type="text" name="name" id={nameFieldId} />
+          <ErrorMessage name="name" className={css.error} component="span" />
+        </div>
 
-             <div className={css.formGroup}>
-                <label htmlFor={numberFieldId}>Number:</label>
-                <Field className={css.field} type="text" name="number" id={numberFieldId} />
-                <ErrorMessage name="number" className={css.error} component="span" />
-            </div>
+        <div className={css.formGroup}>
+          <label htmlFor={numberFieldId}>Number:</label>
+          <Field className={css.field} type="text" name="number" id={numberFieldId} />
+          <ErrorMessage name="number" className={css.error} component="span" />
+        </div>
 
-            <button className={css.button} type="submit">Add contact</button>
-        </Form>
+        <button className={css.button} type="submit">
+          Add contact
+        </button>
+      </Form>
     </Formik>
-    );
+  );
 };
 
 export default ContactForm;
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,17 +1,12 @@
-import css from './SearchBox.module.css';
+import css from "./SearchBox.module.css";
 
 const SearchBox = ({ value, onChange }) => {
-  return ( 
+  return (
     <div className={css.search}>
       <p>Find contacts by name</p>
-      <input
-        className={css.input} 
-        type="text"
-        value={value}
-        onChange={evt => onChange(evt.target.value)}
-      />
+      <input className={css.input} type="text" value={value} onChange={(evt) => onChange(evt.target.value)} />
     </div>
   );
 };
 
-export default SearchBox; 
+export default SearchBox;
